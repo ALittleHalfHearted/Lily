3bdd70e7c0eaddf0107530fd5c1cbce98d920c0c
ALittleHalfHearted <33755588+ALittleHalfHearted@users.noreply.github.com>
2018-12-12 17:57:49 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
-	client.user.setActivity('type L!help for commands');
+	client.user.setActivity('If you need help, L!ask');
 });
 
 client.on('message', message => {
