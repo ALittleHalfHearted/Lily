f2a202b4a32cd229dc32be9fbb6563ffb490f74f
ALittleHalfHearted <33755588+ALittleHalfHearted@users.noreply.github.com>
2018-12-13 15:08:27 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ client.on('message', message => {
 					message.reply('Strangers are scary');
 				}else{
 					message.reply('all hail the creator');
-					console.log(message.author.dmChannel);
+					console.log(JSON.parse("package.json"));
 					//OwnerDM.send('Testing');
 				}
 			break;
