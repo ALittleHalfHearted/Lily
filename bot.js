2af36799ff4f1e14898c02954019b1c5b2d8c149
ALittleHalfHearted <33755588+ALittleHalfHearted@users.noreply.github.com>
2018-12-13 14:20:49 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,10 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const Creator = user.id = 220176861379035137;
 
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
 	client.user.setActivity('If you need help, L!ask');
-	Creator.createDM().message('Ready!');
+	//220176861379035137.createDM().message('Ready!');
 });
 
 client.on('message', message => {
