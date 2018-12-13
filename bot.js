dd503acb0d00b61c63a18fd6d1f6cfdbcfa735c3
ALittleHalfHearted <33755588+ALittleHalfHearted@users.noreply.github.com>
2018-12-13 15:55:22 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 const OwnerID = 220176861379035137;
 const OwnerDM = 522870604353765386;
 var fs = require("fs");
-var contents = fs.readFileSync("jsoncontent.json");
+var contents = fs.readFileSync("package.json");
 
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
