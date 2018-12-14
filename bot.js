450af427002f726e4429abb6410acdc5a311ebde
ALittleHalfHearted <33755588+ALittleHalfHearted@users.noreply.github.com>
2018-12-14 09:59:48 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,7 +18,7 @@ client.on('message', message => {
 		args = args.splice(1).toString().replace(/,/g,' ');
 		
 		switch(cmd){
-			case 'owner':
+			/*case 'owner':
 				console.log(message.author.id);
 				if(message.author.id != DMOwner.recipient.User.id){
 					message.reply('Strangers are scary');
@@ -26,20 +26,35 @@ client.on('message', message => {
 					message.reply('all hail the creator');
 					DMOwner.send('bluh');
 				}
-			break;
+			break;*/
 			case 'ping':
 				message.reply('Pong!\n\nIs a fun game, I agree.');
 			break;
 			case 'ask':
 				switch(args){
+					case '1':
+						message.reply('Well my dear, I am so glad you asked! The "pets" are Yashy :yhoe:, who Lucy found as an egg '+
+							      'and has raised since she hatched (and is definitely not a yoshi); Blur, :yeet: the poor soul'+
+							      ' with no memory of his past; and Chirpy, :chirpy: who is Yashy\'s adopted child after she '+
+							      'saved Chirpy\'s egg  from a snake. (I mostly just hang around to keep an eye on everyone.)'+
+							      ' We are part of the webcomic Bittersweet Candy Bowl, and for a long time we were the balancing '+
+							      'forces to keep the comic from becoming too melodromatic, and sometimes I would guide the kids on the'+
+							      ' right path, but after Lucy\'s... Incedent, we have been largely absent. Perhaps to allow for a more intense plotline?');
+					break;
 					default:
 						message.reply('The following questions are some of the most common ones people ask me'+
-							      ':\n\n:one: | sample text\n:asterisk: | Suggest a question or feature!\n'+
+							      ':\n\n:one: | What/who are "the pets"?\n'//:asterisk: | Suggest a question or feature!\n'+
 							      'Ask your question by typing `L!ask #` and I will respond right away.\n\n' +
 							      'You can also ask for various information regarding my friends and I'+
-							      ' with `L!fact`. If you are more interested in a casual, polite conversation,'+
-							      'I would be happy to oblige with `L!day`.');
+							      ' with `L!fact`. If you are more interested in a casual, polite'+
+							      ' conversation, I would be happy to oblige with `L!day`.');
 				}
+			break;
+			case 'fact':
+				message.reply(':bask: Did you know? :bask:\nThis function is a work in progress! I\'ll do a bit more research, then get back to you.');
+			break;
+			case 'day':
+				message.reply('Today, I\'ve been rather busy with being born, I\'ll let you know when I\'ve had time to adjust.');
 			break;
 			default:
 				message.reply('Sorry dear, I didn\'t quite catch that. Could you repeat yourself?');
